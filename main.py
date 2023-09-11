a8b2f3259a33156af7ce839e69973fa2867b534e
Muxite <muk.chunpongtong@gmail.com>
2023-09-10 20:37:30 -0600

    functionalish

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import pprint
 import random
+import math
 
 pp = pprint.PrettyPrinter(indent=3)
 
@@ -10,15 +11,18 @@ def update_caret(string):
     index = string.find('|')  # return the location of the caret, -1 if none
     if index == -1:
         new_string = '|' + string  # add a caret at the front
+        print(new_string)
         return new_string
     else:
         if index+2 < len(string):  # if have not reached the end of the string
             string = string.replace('|', '')
             new_string = string[:index+1] + '|' + string[index+1:]
+            print(new_string)
             return new_string
         else:
             string = string.replace('|', '')  # remove the carat
             new_string = '|' + string  # loop the caret back to the start
+            print(new_string)
             return new_string
 
 
@@ -45,9 +49,11 @@ def mutate(organism):
 
 class Simulation:  # this class is the conversation itself
     def __init__(self):
-        self.name = str(input("NAME OF THE SIMULATION: "))
-        self.seed_location = str(input("SEED FOLDER LOCATION: ")).replace('"', '')
-        self.results_location = str(input("RESULTS FOLDER LOCATION (CAN BE CREATED): ")).replace('"', '')
+        #self.name = str(input("NAME OF THE SIMULATION: "))
+        #self.seed_location = str(input("SEED FOLDER LOCATION: ")).replace('"', '')
+        #self.results_location = str(input("RESULTS FOLDER LOCATION: ")).replace('"', '')
+        self.seed_location = 'F:\in'
+        self.results_location = 'F:\out'
         self.turns = int(input("MAX TURNS IN SIMULATION: "))
         self.organisms = []  # this is the big file where all the organisms are
         self.difficulty = 1
@@ -58,73 +64,87 @@ class Simulation:  # this class is the conversation itself
             for i in range(len(txt_files)):
                 with open(os.path.join(self.seed_location, txt_files[i]), 'r') as f:
                     self.organisms.append(f.read())  # read the contents of the file and append it to the list
-                    self.organisms[i] = update_caret(self.organisms[i])  # add carets
             pp.pprint(self.organisms)
         except FileNotFoundError:
             input("ERROR: FOLDER NOT FOUND, CREATE AND POPULATE THE FOLDER, THEN RESTART SIMULATION")
 
     def update(self):
+        for o in range(len(self.organisms)):
+            self.organisms[o] = update_caret(self.organisms[o])
         old = self.organisms.copy()  # read from old, write to self.organism
         for o in range(len(old)):  # organism is a string
-            update_caret(self.organisms[o])
-            index = old[o].find('|')+1  # the character after the caret
+
+            index = old[o].find('|')
             old[o] = old[o].replace('|', '')  # remove it to avoid mistakes
             # clone 'c'
             if old[o][index] == 'c':
                 # clone is viable early, but loses viability long term as the cost increases
-                if self.difficulty > 1:  # will require more than 1 c to clone
-                    # check backwards
-                    do_clone = True
-                    for i in range(wrap(index-self.difficulty+1, len(old[o])), index):
+                # check backwards
+                do_clone = True
+                try:
+                    print(range(index-self.difficulty+1, index))
+                    for i in range(index-self.difficulty+1, index):
                         if old[o][wrap(i, len(old[o]))] != 'c':
                             do_clone = False
-                            break  # failed cloning
+                            print("failed to clone")
                     if do_clone:
                         # if that all worked, then clone one
                         print("cloned with " + str(self.difficulty))
                         self.organisms.append(mutate(old[o]))
-                else:
-                    print("cloned")
-                    self.organisms.append(mutate(old[o]))
+                except IndexError:
+                    print("failed to clone: IndexError")
 
             # kill 'k'
             if old[o][index] == 'k':
                 # select a random to kill
-                target = random.randint(0, len(old))
-                print("killed " + str(target))
-                self.organisms.remove(target)  # kills one, possibly itself
+                target = random.randint(0, len(old)-1)
+                try:
+                    print("killed " + str(target))
+                    self.organisms.remove(self.organisms[target])  # kills one, possibly itself
+                except IndexError:
+                    print("failed kill")
 
             # inject 'i'
             if old[o][index] == 'i':
                 # select a random to inject to
-                target = random.randint(0, len(old))
+                target = random.randint(0, len(old)-1)
                 # select a portion to inject
                 to_inject = select_random_section(old[o])
-                print("injected " + str(to_inject) + " to " + str(target))
-                self.organisms[target] += mutate(to_inject)  # add the injection to end
+                try:
+                    print("injected " + str(to_inject) + " to " + str(target))
+                    self.organisms[target] += mutate(to_inject)  # add the injection to end
+                except IndexError:
+                    print("failed injection")
 
             # steal 's'
             if old[o][index] == 's':
                 # select a random to steal from
-                target = random.randint(0, len(old))
+                target = random.randint(0, len(old)-1)
                 # select a portion to inject
                 to_steal = select_random_section(old[target])
-                print("stolen " + str(to_steal) + " from " + str(target))
-                self.organisms[o] += mutate(to_steal)  # add the injection to end of future self
+                try:
+                    print("stolen " + str(to_steal) + " from " + str(target))
+                    self.organisms[o] += mutate(to_steal)  # add the injection to end of future self
+                except IndexError:
+                    print("failed steal")
 
             # reinforce 'r'
             if old[o][index] == 'r':
                 # take from self
                 to_reinforce = select_random_section(old[o])
-                print("reinforced " + str(to_reinforce))
-                self.organisms[o] += mutate(to_reinforce)  # add to future self
+                try:
+                    print("reinforced " + str(to_reinforce))
+                    self.organisms[o] += mutate(to_reinforce)  # add to future self
+                except IndexError:
+                    print("failed reinforce")
 
     def update_difficulty(self):
         if len(self.organisms) < 100:
             self.difficulty = 1
+            print("difficulty =" + str(1 + 0.001 * pow(len(self.organisms), 1.1)))
         else:
-            self.difficulty = int(1 + 0.0001*pow(len(self.organisms), 1.8))
-            print("difficulty =" + str(1 + 0.0001*pow(len(self.organisms), 1.8)))
+            self.difficulty = math.floor(1 + 0.001*pow(len(self.organisms), 1.1))
+            print("difficulty =" + str(1 + 0.001*pow(len(self.organisms), 1.1)))
 
     def save_txt(self, max_files):
         for i in range(0, len(self.organisms)):
